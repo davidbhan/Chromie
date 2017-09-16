0dc951b197519c92b15513c56b1b320915fb74da
juwonlee1020 <juwonlee1020@gmail.com>
2017-09-16 08:49:03 -0400

    Updated APIAI

--- popup.js
+++ popup.js
@@ -1,8 +1,17 @@
 $(function(){
     console.log("testing_1");
-    
     const client = new ApiAi.ApiAiClient({accessToken: 'd82da7b4ba744715a6c726a03b58147a'});
-
+    
+    var first_run = true;
+    if(!localStorage['ran_before']){
+        first_run = true;
+        localStorage["ran_before"] = "1";
+    }
+    if(first_run){
+          $("#output").append($("<p class='bot_resp'>Hi there! My name is Chromie, and I'm here to help you be on top of your college academics.</p>"));
+          $("#output").append($("<p class='bot_resp'>There are a few things I need to know about you to get started. If you're ready, type in 'Ready'!</p>"))    
+    }    
+    
     $("#input").keyup(function(event){
         if(event.keyCode == 13){
             $("#submitButton").click();
@@ -10,9 +19,8 @@ $(function(){
     });
 
     $("#submitButton").click(function(){
-        var currentdate = new Date();
         var text_req = $("#queryString").val();
-        $("#output").append($("<p class='user_resp'>" + text_req +"</p>"));
+        $("#output").append($("<p class='user_resp'>" + text_req +"</p>" ));
         let promise = client.textRequest(text_req);
         promise.then(handleResponse);
         $("#queryString").val('');
