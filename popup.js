47cf5f76f8fd11d5679f7d38565423b1f7db7535
juwonlee1020 <juwonlee1020@gmail.com>
2017-09-16 03:37:02 -0400

    Integrated w/ API.AI

--- popup.js
+++ popup.js
@@ -1,16 +1,29 @@
+/*
 const client = new ApiAi.ApiAiClient({accessToken:"d82da7b4ba744715a6c726a03b58147a"});
 var promise = client.textRequest("Hi, what's your name?");
 
 $(function(){
-    $("#submitButton").click(function(){
-        promise.then(handleResponse)
-                .catch(handleError);
-    })
+   $("#submitButton").click(function(){
+       
+   })
+     
 })
+*/
 
-function handleResponse(serverResponse) {
-        console.log(serverResponse);
-}
-function handleError(serverError) {
-        console.log(serverError);
-}
+$(function(){
+    console.log("testing_1");
+    const client = new ApiAi.ApiAiClient({accessToken: 'd82da7b4ba744715a6c726a03b58147a'});
+    
+    $("#submitButton").click(function(){
+        var text_req = $("#queryString").val();
+        let promise = client.textRequest(text_req);
+        promise.then(handleResponse);
+    });
+    function handleResponse(serverResponse){
+        var obj = JSON.stringify(serverResponse.result.fulfillment.speech);
+        /*var response = obj.result.fulfillment.speech;*/
+        $("#test").text(obj);
+        console.log(JSON.stringify(serverResponse));
+    }
+    
+})
