41d7db2e9fba3be12a103a1971e72adba0e1d8dd
David B Han <davidhan321@gmail.com>
2017-09-16 04:34:15 -0400

    enter function

--- popup.js
+++ popup.js
@@ -2,14 +2,20 @@ $(function(){
     console.log("testing_1");
     const client = new ApiAi.ApiAiClient({accessToken: 'd82da7b4ba744715a6c726a03b58147a'});
     
+    $("#input").keyup(function(event){
+        if(event.keyCode == 13){
+            $("#submitButton").click();
+        }
+    });
+
     $("#submitButton").click(function(){
         var text_req = $("#queryString").val();
         $("#output").append($("<p class='user_resp'>" + text_req +"</p>"));
         let promise = client.textRequest(text_req);
         promise.then(handleResponse);
+        $("#queryString").val('');
     });
-    
-    
+
     
     function handleResponse(serverResponse){
         var obj = JSON.stringify(serverResponse);
