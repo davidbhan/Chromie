17abb6b5c53b6239c7bcb97384af554952496dc1
juwonlee1020 <juwonlee1020@gmail.com>
2017-09-16 04:15:49 -0400

    Creating UI

--- popup.js
+++ popup.js
@@ -1,29 +1,24 @@
-/*
-const client = new ApiAi.ApiAiClient({accessToken:"d82da7b4ba744715a6c726a03b58147a"});
-var promise = client.textRequest("Hi, what's your name?");
-
-$(function(){
-   $("#submitButton").click(function(){
-       
-   })
-     
-})
-*/
-
 $(function(){
     console.log("testing_1");
     const client = new ApiAi.ApiAiClient({accessToken: 'd82da7b4ba744715a6c726a03b58147a'});
     
     $("#submitButton").click(function(){
         var text_req = $("#queryString").val();
+        $("#output").append($("<p class='user_resp'>" + text_req +"</p>"));
         let promise = client.textRequest(text_req);
         promise.then(handleResponse);
     });
+    
+    
+    
     function handleResponse(serverResponse){
-        var obj = JSON.stringify(serverResponse.result.fulfillment.speech);
+        var obj = JSON.stringify(serverResponse);
+        var bot_response = JSON.stringify(serverResponse.result.fulfillment.speech).slice(1,-1);
         /*var response = obj.result.fulfillment.speech;*/
         $("#test").text(obj);
+        $("#output").append($("<p class='bot_resp'>" + bot_response + "</p>"))
         console.log(JSON.stringify(serverResponse));
+        
     }
     
 })
