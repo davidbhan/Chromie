818c10e3f5e5e63cfb7cc37546c4721c2ef942b2
juwonlee1020 <juwonlee1020@gmail.com>
2017-09-17 02:25:47 -0400

    dklj

--- popup.js
+++ popup.js
@@ -9,8 +9,11 @@ $(function(){
     }
     if(first_run){
           $("#output").append($("<p class='bot_resp'>Hi there! My name is Chromie, and I'm here to help you be on top of your college academics.</p>"));
-          $("#output").append($("<p class='bot_resp'>There are a few things I need to know about you to get started. If you're ready, type in 'Ready'!</p>"))    
+          $("#output").append($("<p class='bot_resp'>There are a few things I need to know about you to get started. If you're ready, type in 'Ready'!</p>"));
+          
     }    
+    
+    
     
     $("#input").keyup(function(event){
         if(event.keyCode == 13){
@@ -22,7 +25,23 @@ $(function(){
         var text_req = $("#queryString").val();
 
         $("#output").append($("<p class='user_resp'>" + text_req +"</p>"));
-
+        
+        if(text_req == "yes"){
+              console.log("yess");
+            
+            var data = { 
+                "name":"courses_3",
+                "entries":[{
+                "value":"math_2",
+                "synonyms":["math"]
+                }]
+            };
+            
+            callApiAi('POST', 'entities', data);
+        
+          }
+        
+        
         let promise = client.textRequest(text_req);
         promise.then(handleResponse);
 
@@ -31,6 +50,20 @@ $(function(){
         var objDiv = document.getElementById("output");
         objDiv.scrollTop = objDiv.scrollHeight;
     });
+    
+    function callApiAi(method, url, data, onComplete) {
+        $.ajax({
+          method: method,
+          url: 'https://api.api.ai/v1/' + url + '?v=20150910',
+          dataType: "json",
+          contentType: "application/json",
+          headers: {
+            'Authorization': 'Bearer 277d596d38d443089664255f27ef1ee6'
+          },
+          data: method === 'GET' ? null : JSON.stringify(data),
+          complete: onComplete
+        });
+    }
 
  
     function handleResponse(serverResponse){
